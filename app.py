4d51b1bc2922e737b6c931dc63a93d0ff0ed8b6a
Mitesh Nandan <nandanmitesh@gmail.com>
2024-11-10 15:02:28 +0530

    longchain_community removed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import StuffDocumentsChain, LLMChain
 from langchain.prompts import PromptTemplate
